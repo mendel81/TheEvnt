a3187ff0d4263fc5d15bf34dc970aa52022c5e07
Fernando J. Pinilla <hola@fernandopinilla.com>
2012-11-15 18:00:34 +0100

    Initial Commit

---
+++ TheEvnt/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  TheEvnt
+//
+//  Created by Synapse Asesores Informaticos, S.L. on 15/11/12.
+//  Copyright (c) 2012 Synapse Asesores Informaticos, S.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ TheEvnt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TheEvnt
+//
+//  Created by Synapse Asesores Informaticos, S.L. on 15/11/12.
+//  Copyright (c) 2012 Synapse Asesores Informaticos, S.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
